2c0f8ad63eee9834e2c62101b038b81511a5acbc
Peter Friese <peter@peterfriese.de>
2021-05-25 22:00:44 +0200

    🧹 Cleanup
    Signed-off-by: Peter Friese <peter@peterfriese.de>

--- AsyncAwaitFirebase/backend/_stuff/functions/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as functions from "firebase-functions";
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onCall((data, context) => {
-  functions.logger.info("Hello logs!", {structuredData: true})
-  return {result: "Hello from Firebase!"}
-})

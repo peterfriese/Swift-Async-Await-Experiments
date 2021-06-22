ff50495bb38e3d4afe00226a31a4d110a2f2abce
Peter Friese <peter@peterfriese.de>
2021-06-22 16:06:37 +0200

    🍝 Add example for non-viewmodel-based code.
    Signed-off-by: Peter Friese <peter@peterfriese.de>

--- WordBrowser/WordBrowser/Views/WordDetailsViewNoViewModel.swift
+++ WordBrowser/WordBrowser/Views/WordDetailsViewNoViewModel.swift
@@ -8,13 +8,63 @@
 import SwiftUI
 
 struct WordDetailsViewNoViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State var word: String
+  @State var isSearching = false
+  @State var definitions = [Definition]()
+  
+  private func buildURLRequest(for term: String) -> URLRequest {
+    let escapedSearchTerm = term.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
+    let url = URL(string: "https://wordsapiv1.p.rapidapi.com/words/\(escapedSearchTerm)/definitions")!
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    request.setValue(wordsAPIKey, forHTTPHeaderField: apiKeyHeader)
+    request.setValue(wordsAPIHost, forHTTPHeaderField: apiHostHeader)
+    return request
+  }
+  
+  func search(for term: String) async -> Word {
+    // build the request
+    let request = buildURLRequest(for: term)
+    
+    do {
+      let (data, response) = try await URLSession.shared.data(for: request)
+      guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+        throw WordsAPI.invalidServerResponse
+      }
+      let word = try JSONDecoder().decode(Word.self, from: data)
+      return word
     }
+    catch {
+      return Word.empty
+    }
+  }
+  
+  var body: some View {
+    ZStack {
+      if isSearching {
+        ProgressView("Fetching...")
+      }
+      else {
+        List {
+          Section("Definitions") {
+            ForEach(definitions) { definition in
+              DefinitionView(definition: definition)
+            }
+          }
+          .lineLimit(2)
+        }
+      }
+    }
+    .navigationTitle(word)
+    .task {
+      let result = await search(for: word)
+      definitions = result.definitions ?? []
+    }
+  }
 }
 
 struct WordDetailsViewNoViewModel_Previews: PreviewProvider {
-    static var previews: some View {
-        WordDetailsViewNoViewModel()
-    }
+  static var previews: some View {
+    WordDetailsViewNoViewModel(word: "Swift")
+  }
 }
